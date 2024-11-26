14d25331b76c0e6f1283e85d7067c1a51c4952fc
MattHudzikiewicz <144395423+MattHudzikiewicz@users.noreply.github.com>
2024-11-25 20:07:11 -0500

    Added dialog box for delete button on lasagna card (#7)

--- app/src/main/java/com/mobileapp/recipepal/RecipeFragment.java
+++ app/src/main/java/com/mobileapp/recipepal/RecipeFragment.java
@@ -1,5 +1,6 @@
 package com.mobileapp.recipepal;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,11 +10,15 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mobileapp.recipepal.databinding.FragmentRecipeBinding;
 
 public class RecipeFragment extends Fragment {
     private FragmentRecipeBinding binding;
+
+    Dialog dialog;
+    Button buttonDialogCancel, buttonDialogDelete;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +27,32 @@ public class RecipeFragment extends Fragment {
 
         View view = binding.getRoot();
         // Inflate the layout for this fragment
+
+        dialog = new Dialog(binding.getRoot().getContext());
+        dialog.setContentView(R.layout.dialog_box);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(getActivity().getDrawable(R.drawable.dialog_background));
+        dialog.setCancelable(false);
+
+        buttonDialogCancel = dialog.findViewById(R.id.cancelDeleteButton);
+        buttonDialogDelete = dialog.findViewById(R.id.confirmDeleteButton);
+
+        buttonDialogCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        buttonDialogDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+
+
+
 
         binding.addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +78,12 @@ public class RecipeFragment extends Fragment {
                 NavDirections action
                         = RecipeFragmentDirections.actionRecipeFragmentToDetailedRecipeFragment();
                 Navigation.findNavController(binding.getRoot()).navigate(action);
+            }
+        });
+        binding.deleteRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
             }
         });
 
